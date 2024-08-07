4fd46930220ab63162f630d18de86fc8e17a49b7
elimf <90334513+elimf@users.noreply.github.com>
2024-08-08 01:56:15 +0200

    chore: Refactor RoomManager component button classnames for consistency

--- src/component/Waiting.js
+++ src/component/Waiting.js
@@ -37,14 +37,7 @@ const RoomManager = ({
             <li key={room.id} className="room-list-item">
               {room.name}{" "}
               <button
-                className="
-    px-4 py-2
-    bg-blue-500 text-white
-    rounded-lg
-    hover:bg-blue-600
-    focus:outline-none focus:ring-2 focus:ring-blue-500
-    transition duration-300 ease-in-out
-  "
+                className=" px-4 py-2 bg-blue-500 text-white rounded-lg hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 transition duration-300 ease-in-out"
                 onClick={() => onJoinRoom(room.id)}
               >
                 Rejoindre la salle
