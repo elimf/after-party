f83a68e243387f05e3d76922c06a29ee9d27efc5
elimf <90334513+elimf@users.noreply.github.com>
2024-08-09 02:32:01 +0200

    Refactor Connect.css and Chat.js for consistent styling and improved user experience

--- src/component/RoomManager.js
+++ src/component/RoomManager.js
@@ -1,22 +1,22 @@
-import React, { useState } from 'react';
-import Modal from 'react-modal';
-import Chat from './Chat'; // Assurez-vous que ce chemin est correct
-import QuizQuestion from './QuizQuestion';
-import QuizResults from './QuizResults';
+import React, { useState } from "react";
+import Modal from "react-modal";
+import Chat from "./Chat"; // Assurez-vous que ce chemin est correct
+import QuizQuestion from "./QuizQuestion";
+import QuizResults from "./QuizResults";
 
-const RoomManager = ({ 
-  currentRoom, 
-  currentUser, 
-  startQuiz, 
-  handleAnswerQuiz, 
-  quizStarted, 
+const RoomManager = ({
+  currentRoom,
+  currentUser,
+  startQuiz,
+  handleAnswerQuiz,
+  quizStarted,
   sendMessage,
   // startPetitBac // Ajoutez une nouvelle prop pour démarrer le Petit Bac
 }) => {
   const [isModalOpen, setIsModalOpen] = useState(false);
-  const [questionType, setQuestionType] = useState('');
-  const [questionCount, setQuestionCount] = useState('');
-  const [difficulty, setDifficulty] = useState('');
+  const [questionType, setQuestionType] = useState("");
+  const [questionCount, setQuestionCount] = useState("");
+  const [difficulty, setDifficulty] = useState("");
   const [isPetitBacActive, setIsPetitBacActive] = useState(false); // État pour savoir si le Petit Bac est actif
   const [isChatOpen, setIsChatOpen] = useState(false);
 
@@ -30,7 +30,7 @@ const RoomManager = ({
     }
   };
 
-  const toggleChat = () => setIsChatOpen(prevState => !prevState);
+  const toggleChat = () => setIsChatOpen((prevState) => !prevState);
 
   const handleStartPetitBac = () => {
     // Assurez-vous que la fonction startPetitBac est définie pour démarrer le jeu du Petit Bac
@@ -42,12 +42,21 @@ const RoomManager = ({
     <div className="relative min-h-screen flex">
       <div className="games-section flex-grow p-4 md:p-6 lg:p-8">
         <div className="flex flex-col gap-4">
-          {currentRoom.ownerId === currentUser.id && !currentRoom.quiz.isRunning && !isPetitBacActive ? (
+          {currentRoom.ownerId === currentUser.id &&
+          !currentRoom.quiz.isRunning &&
+          !isPetitBacActive ? (
             <div className="buttons-grid grid grid-cols-1 sm:grid-cols-2 gap-4">
-              <button onClick={openModal} className="start-quiz-button bg-blue-500 text-white py-2 px-4 rounded-lg hover:bg-blue-600 transition duration-200">
+              <button
+                onClick={openModal}
+                className="start-quiz-button bg-blue-500 text-white py-2 px-4 rounded-lg hover:bg-blue-600 transition duration-200"
+              >
                 Commencer le Quiz
               </button>
-              <button onClick={handleStartPetitBac} className="start-petitbac-button bg-green-500 text-white py-2 px-4 rounded-lg hover:bg-green-600 transition duration-200" disabled>
+              <button
+                onClick={handleStartPetitBac}
+                className="start-petitbac-button bg-green-500 text-white py-2 px-4 rounded-lg hover:bg-green-600 transition duration-200 disabled:bg-gray-400 disabled:cursor-not-allowed disabled:opacity-50"
+                disabled
+              >
                 Commencer le Petit Bac
               </button>
             </div>
@@ -59,21 +68,33 @@ const RoomManager = ({
               quizStarted={quizStarted}
             />
           ) : null}
-          {!quizStarted && !currentRoom.quiz.isRunning && !isPetitBacActive && currentRoom.quiz.results ? (
+          {!quizStarted &&
+          !currentRoom.quiz.isRunning &&
+          !isPetitBacActive &&
+          currentRoom.quiz.results ? (
             <QuizResults quiz={currentRoom.quiz} />
           ) : null}
           {isPetitBacActive && (
             <div className="petit-bac-section bg-gray-100 p-4 rounded-lg shadow-md">
               {/* Contenu du jeu du Petit Bac ici */}
-              <p className="text-lg font-medium">Le jeu du Petit Bac est en cours...</p>
+              <p className="text-lg font-medium">
+                Le jeu du Petit Bac est en cours...
+              </p>
             </div>
           )}
         </div>
       </div>
       {/* Chat Section */}
-      <div className={`fixed top-0 right-0 bg-gray-800 text-white w-100 h-full transform transition-transform ${isChatOpen ? 'translate-x-0' : 'translate-x-full'}`}>
-        <button className="absolute top-4 right-4 bg-gray-700 text-white p-2 w-52 rounded-full hover:bg-gray-600" onClick={toggleChat}>
-          {isChatOpen ? 'Cacher le Chat' : 'Voir Chat'}
+      <div
+        className={`fixed top-0 right-0 bg-gray-800 text-white w-100 h-full transform transition-transform ${
+          isChatOpen ? "translate-x-0" : "translate-x-full"
+        }`}
+      >
+        <button
+          className="absolute top-4 right-4 bg-gray-700 text-white p-2 w-52 rounded-full hover:bg-gray-600"
+          onClick={toggleChat}
+        >
+          {isChatOpen ? "Cacher le Chat" : "Voir Chat"}
         </button>
         {isChatOpen && <Chat room={currentRoom} onSendMessage={sendMessage} />}
       </div>
@@ -86,8 +107,14 @@ const RoomManager = ({
         className="modal max-w-lg mx-auto bg-white p-6 rounded-lg shadow-lg"
         overlayClassName="modal-overlay fixed inset-0 bg-black bg-opacity-50"
       >
-        <h2 className="text-2xl font-semibold mb-4">Choisissez le type de question</h2>
-        <select value={questionType} onChange={(e) => setQuestionType(e.target.value)} className="mb-4 p-2 border border-gray-300 rounded-lg w-full">
+        <h2 className="text-2xl font-semibold mb-4">
+          Choisissez le type de question
+        </h2>
+        <select
+          value={questionType}
+          onChange={(e) => setQuestionType(e.target.value)}
+          className="mb-4 p-2 border border-gray-300 rounded-lg w-full"
+        >
           <option value="">Sélectionner</option>
           <option value="all">Aléatoire</option>
           <option value="Culture Générale">Culture Générale</option>
@@ -99,8 +126,14 @@ const RoomManager = ({
           <option value="Blindtest">Blindtest</option>
         </select>
 
-        <h2 className="text-2xl font-semibold mb-4">Choisissez le nombre de questions</h2>
-        <select value={questionCount} onChange={(e) => setQuestionCount(e.target.value)} className="mb-4 p-2 border border-gray-300 rounded-lg w-full">
+        <h2 className="text-2xl font-semibold mb-4">
+          Choisissez le nombre de questions
+        </h2>
+        <select
+          value={questionCount}
+          onChange={(e) => setQuestionCount(e.target.value)}
+          className="mb-4 p-2 border border-gray-300 rounded-lg w-full"
+        >
           <option value="">Sélectionner</option>
           <option value="5">5</option>
           <option value="10">10</option>
@@ -108,8 +141,14 @@ const RoomManager = ({
           <option value="20">20</option>
         </select>
 
-        <h2 className="text-2xl font-semibold mb-4">Choisissez la difficulté</h2>
-        <select value={difficulty} onChange={(e) => setDifficulty(e.target.value)} className="mb-4 p-2 border border-gray-300 rounded-lg w-full">
+        <h2 className="text-2xl font-semibold mb-4">
+          Choisissez la difficulté
+        </h2>
+        <select
+          value={difficulty}
+          onChange={(e) => setDifficulty(e.target.value)}
+          className="mb-4 p-2 border border-gray-300 rounded-lg w-full"
+        >
           <option value="">Sélectionner</option>
           <option value="easy">Facile</option>
           <option value="medium">Moyenne</option>
@@ -117,10 +156,19 @@ const RoomManager = ({
         </select>
 
         <div className="flex gap-4">
-          <button onClick={handleStartQuiz} disabled={!questionType || !questionCount || !difficulty} className="btn btn-primary bg-blue-500 text-white py-2 px-4 rounded-lg hover:bg-blue-600 transition duration-200">
+          <button
+            onClick={handleStartQuiz}
+            disabled={!questionType || !questionCount || !difficulty}
+            className="btn btn-primary bg-blue-500 text-white py-2 px-4 rounded-lg hover:bg-blue-600 transition duration-200"
+          >
             Commencer le Quiz
           </button>
-          <button onClick={closeModal} className="btn btn-secondary bg-gray-500 text-white py-2 px-4 rounded-lg hover:bg-gray-600 transition duration-200">Annuler</button>
+          <button
+            onClick={closeModal}
+            className="btn btn-secondary bg-gray-500 text-white py-2 px-4 rounded-lg hover:bg-gray-600 transition duration-200"
+          >
+            Annuler
+          </button>
         </div>
       </Modal>
     </div>
