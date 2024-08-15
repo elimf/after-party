0598a0fc93ec57b2b330cc916e1082341e6e15cd
elimf <90334513+elimf@users.noreply.github.com>
2024-08-15 19:06:38 +0200

    feat: Add loading state and disable submit button during login process

--- src/component/Connect.js
+++ src/component/Connect.js
@@ -1,21 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { toast } from 'react-toastify';
 import { useAuth } from '../hooks/AuthContext';
-import { useNavigate} from "react-router-dom"; 
+import { useNavigate } from "react-router-dom"; 
 
 const Connect = ({ onConnect }) => {
+  const [loading, setLoading] = useState(false); // Ajout de l'état de chargement
   const token = localStorage.getItem('authToken');  
   const { logout } = useAuth();
   const navigate = useNavigate();
+
   const handleConnect = () => {
+    setLoading(true); // Début du chargement
+
     if (!token) {
       logout();
-      navigate('/login');
-      toast.error('Vous devez vous connecter pour accéder à cette page, vous allez être redirigé vers la page de connexion').then(() => {
-        window.location.href = '/login';
-      });
+      toast.error('Vous devez vous connecter pour accéder à cette page, vous allez être redirigé vers la page de connexion');
+      
+      setTimeout(() => {
+        navigate('/login');
+        setLoading(false); // Fin du chargement après la redirection
+      }, 3000); // Attend 3 secondes avant de rediriger
     } else {
       onConnect(token);
+      setLoading(false); // Fin du chargement après l'appel de onConnect
     }
   };
 
@@ -27,9 +34,10 @@ const Connect = ({ onConnect }) => {
         </h1>
         <button
           onClick={handleConnect}
-          className="bg-blue-500 text-white text-xl py-4 px-8 rounded-lg hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50 transition duration-200"
+          className={`bg-blue-500 text-white text-xl py-4 px-8 rounded-lg hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50 transition duration-200 ${loading ? 'opacity-50 cursor-not-allowed' : ''}`}
+          disabled={loading} // Désactive le bouton pendant le chargement
         >
-          Connexion à After Party
+          {loading ? "Chargement..." : "Connexion à After Party"} {/* Affiche "Chargement..." pendant l'exécution */}
         </button>
       </div>
     </div>
